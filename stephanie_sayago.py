6bff7e095451006616653515b59acadfaffa0822
steph <steph.sayago@gmail.com>
2020-11-02 22:00:23 -0300

    final

--- stephanie_sayago.py
+++ stephanie_sayago.py
@@ -1,7 +1,33 @@
 import csv
 import os.path
 
-def guardarDatos(archivo, campos):
+def modificar(archivo, campos):
+    lista_Empleados = cargarLista(archivo, campos)
+    try:
+        with open(archivo, 'a', newline='') as file:
+            file_guarda = csv.DictWriter(file, fieldnames=campos)
+            file_guarda.writerows(lista_Empleados)
+            print(f"se guardo correctamente {archivo}")
+            return
+    except IOError:
+        print("Ocurrio un error con el archivo")
+
+
+def sobreescribir(archivo, campos):
+    lista_Empleados = cargarLista(archivo, campos)
+    try:
+        with open(archivo, 'w', newline='') as file:
+            archivo_guarda = csv.DictWriter(file, fieldnames=campos)
+
+            archivo_guarda.writeheader()
+
+            archivo_guarda.writerows(lista_Empleados)
+            print(f"Se guardo exitosamente {archivo}")
+            return
+    except IOError:
+        print("Ocurrio un error con el archivo")
+
+def cargarLista(archivo, campos):
     cargar = "si"
     lista_empleados = []
     while cargar == "si":
@@ -22,11 +48,31 @@ def guardarDatos(archivo, campos):
                 empleado[campo] = valor     
         lista_empleados.append(empleado)
 
-        cargar = input("Desea seguir agregando empleados? Si/No")
+        cargar = input("Desea seguir agregando empleados? Si/No ")
+    return lista_empleados
 
+def opcionArchivo(archivo,campos):
+    archivo_creado = os.path.isfile(archivo)
+    
+    if archivo_creado:
+        print(f"Ya existe el archivo, ¿desea Modificar o Sobreescribir? m/s")
+        accion = input("")
+        if accion == "s":
+            sobreescribir(archivo, campos)
+            return
+        if accion == "m":
+            modificar(archivo, campos)
+            return
+        else:
+            print('ingrese una opcion valida')
+    else:
+        sobreescribir(archivo, campos)
+        return
+
+def guardarDatos(archivo, campos):
+    cargarLista
     try:
         archivo_existe = os.path.isfile(archivo)
-        print(archivo_existe)
         with open(archivo, 'a', newline='') as file:
             file_guarda = csv.DictWriter(file, fieldnames=campos)
 
@@ -40,6 +86,43 @@ def guardarDatos(archivo, campos):
         print("Ocurrio un error con el archivo")
 
 
+def cargarArchivo(archivo,vacaciones):
+    aux_empleados = open(archivo)
+    aux_vacaciones = open(vacaciones)
+    empleados_csv = csv.reader(aux_empleados)
+    vacaciones_csv = csv.reader(aux_vacaciones)
+
+    #Saltear Encabezados
+    next(empleados_csv)
+    next(vacaciones_csv)
+
+    empleados = next(empleados_csv, None)
+    vacaciones = next(vacaciones_csv, None)
+
+    validar = True
+    while validar:
+        valor = input(f"Ingrese el numero de legajo: ")
+        try:
+            valor = int(valor)
+            validar = False
+        except ValueError:
+            validar = True
+            print(f'El legajo debe ser un numero')
+
+    contador_dias = 0
+    while vacaciones:
+        if int(vacaciones[0]) == valor:
+            contador_dias += 1
+        vacaciones = next(vacaciones_csv, None)
+    
+    while empleados:
+        if int(empleados[0]) == valor:
+            total_vacaciones = int(empleados[3])
+            dias_restantes = total_vacaciones - int(contador_dias)
+            print(f'Legajo {valor}: {empleados[1]} {empleados[2]}, le restan {dias_restantes} dias de vacaciones')    
+  
+        empleados = next(empleados_csv, None)
+
 def inicio():
     print("Ingrese el nombre del archivo")
     archivo = input("")
@@ -52,9 +135,9 @@ def inicio():
 
         if opcion == "3":
             exit()
-        if opcion == "1":
-            guardarDatos(archivo, CAMPOS)
-        if opcion == "2":
+        elif opcion == "1":
+            opcionArchivo(archivo, CAMPOS)
+        elif opcion == "2":
             cargarArchivo(archivo,VACACIONES)
         else:
             print("Por favor elija una opcion valida")
